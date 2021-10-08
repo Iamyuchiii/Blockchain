817310979f8f0be2779ecb088d7af5d6bf5237da
Yuchen Huang <yuchen520@live.nl>
2021-10-08 17:28:29 +0200

    Functionality change
    Block class is now able to hash itself with a difficulty grade
    
    Proof_of_work function now checks the hashs of currrent block and previous block for validation before adding to the chain (not finished still need some work)

--- blockchain_learn.py
+++ blockchain_learn.py
@@ -4,23 +4,34 @@ import json
 from datetime import datetime
 from merkle_tree import MerkleTools
 from tools import *
+from copy import deepcopy
 
 # TODO -----------------------------------------------------------------------
 # smart contract
+# - performs certain actions after meeting the requirement
 # certificate TLS / SSL = time limit - refresh token
+# - optional for servers
+# add fucntion where the info is added after being valid
 # TODO -----------------------------------------------------------------------
 
 class Block:
-    def __init__(self, index, previous_hash, merkle_tree, timestamp, nonce):
+    def __init__(self, index, previous_hash, merkle_tree, timestamp, difficulty):
         self.index = index
         self.previous_hash = previous_hash
         self.timestamp = timestamp
-        self.nonce = nonce
+        self.nonce = 0
+        self.difficulty = difficulty
         self.merkle_tree = merkle_tree
         self.merkle_root = self.merkle_tree.get_merkle_root()
         self.block_hash = self.compute_hash()
 
-    def compute_hash(self):
+    def __str__(self):
+        """
+        :return: when print the block object will be printed in dictionary
+        """
+        return str(self.__dict__)
+
+    def hashing(self):
         """
         Makes double hash from the information store in the block in form of
         dictionary
@@ -36,12 +47,18 @@ class Block:
         second_hash = sha256(first_hash.encode()).hexdigest()
         return second_hash
 
+    def compute_hash(self):
+        hash = self.hashing()
+        while int(hash, 16) > (2 ** (256 - self.difficulty)):
+                self.nonce += 1
+                # reseting this, otherwise using the nonce wont give the same hash
+                # since python cumulate nonce times to get the hash without the reset
+                hash = self.hashing()
+        # while not hash.startswith("0" * difficulty):
+        #     self.nonce += 1
+        #     hash = self.hashing()
+        return hash
 
-    def __str__(self):
-        """
-        :return: when print the block object will be printed in dictionary
-        """
-        return str(self.__dict__)
 
 class Blockchain():
     def __init__(self):
@@ -62,18 +79,20 @@ class Blockchain():
         mt = self.build_merkle_tree({"user_input" : "dummy"})
         # makes first block for as dummy data
         origin_block = Block("Origin", 0x0, mt,
-                             "datetime.now().timestap()",0)
-        origin_block.block_hash = origin_block.compute_hash()
+                             "datetime.now().timestap()", 5)
         # add block to the chain
         self.chain.append(origin_block.block_hash)
         # saves the information of the firstblock as dictionary
-        self.blocks.append(str(origin_block.__dict__))
+        self.blocks.append(origin_block.__dict__)
 
-    def getlastblock(self):
+    def getlasthash(self):
         """
         :return: last hashvalue of the blockchain
         """
         return self.chain[-1]
+
+    def getlastblock(self):
+        return self.blocks[-1]
 
     def proof_of_work(self, block):
         """
@@ -81,20 +100,14 @@ class Blockchain():
         :param block: a block object
         :return: the hash value met by the difficulty
         """
-        difficulty = 5
-        block.nonce = 0
-        hash = block.compute_hash()
-        while int(hash, 16) > (2 ** (256 - difficulty)):
-            block.nonce += 1
-            # reseting this, otherwise using the nonce wont give the same hash
-            # since python cumulate nonce times to get the hash without the reset
-            hash = block.compute_hash()
-        return hash
-        # while not hash.startswith("0" * difficulty):
-        #     block.nonce += 1
-        #     hash = block.compute_hash()
-        # return hash
-        # No diffculty requirement each step
+        # to exclude the current hash to recreate block hash
+        modification = without_keys(block.__dict__, {"merkle_tree", "block_hash"})
+        block_string = json.dumps(modification, sort_keys=True)
+        first_hash = sha256(block_string.encode()).hexdigest()
+        second_hash = sha256(first_hash.encode()).hexdigest()
+        return second_hash == block.block_hash and \
+               self.getlasthash() == block.previous_hash and \
+               int(second_hash, 16) < 2 ** (256 - block.difficulty)
 
     def build_merkle_tree(self, data):
         """
@@ -118,12 +131,14 @@ class Blockchain():
         """
         mt = self.build_merkle_tree(data)
         block = Block(len(self.chain), self.chain[-1], mt,
-                      datetime.now().timestamp(), 0)
-        block.block_hash = self.proof_of_work(block)
-        # add block to the chain
-        self.chain.append(block.block_hash)
-        # saves the information of the firstblock as dictionary
-        self.blocks.append(block.__dict__)
+                      "datetime.now().isoformat()", 5)
+        if self.proof_of_work(block):
+            # add block to the chain
+            self.chain.append(block.block_hash)
+            # saves the information of the firstblock as dictionary
+            self.blocks.append(block.__dict__)
+        else:
+            print("aa")
         return self.blocks[-1]
 
 
@@ -135,8 +150,9 @@ if __name__ == "__main__":
 
     chain = Blockchain()
     chain.add_info(data)
-    for i in chain.blocks:
-        print(i)
+    print(chain.blocks)
+
+
 
 
 # use merkle tree
