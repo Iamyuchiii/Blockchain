6bc65596de7de83eb1f0c73f87f50f79f7f8085c
Yuchen Huang <yuchen520@live.nl>
2021-11-20 03:12:01 +0100

    No changes

--- Data_filtering.py
+++ Data_filtering.py
@@ -1,31 +1,41 @@
 import scipy.stats as stats
 
-data = stats.uniform.rvs(size=100, loc = 40, scale = 80)
-mu, std = stats.norm.fit(data)
-average = sum(data)/len(data)
+class DataSort():
+    def __init__(self, data):
+        """Class for sorting datas
+        :param data:
+        """
+        self.data = data
 
-def make_ranks(data, levels):
-    sorted_data = {}
-    difference = (max(data) - min(data))/levels
-    minimum = min(data)
-    upperscale = minimum + difference
-    for i in range(levels):
-        sorted_data[minimum, upperscale] = []
-        minimum += difference
-        upperscale += difference
+    def sortrank(self, ranks):
+        """divide numbers into selected ranks and place and numbers insde the
+        ranks.
+        :param ranks: number of ranks
+        :return: a dictionary containing sorted ranks
+        """
+        sorted_data = {}
+        difference = (max(self.data) - min(self.data))/ranks
+        minimum = min(self.data)
+        upperscale = minimum + difference
+        for i in range(ranks):
+            sorted_data[minimum, upperscale] = []
+            minimum += difference
+            upperscale += difference
 
-    for weight in data:
-        for key, value in sorted_data.items():
-            if weight >= key[0] and weight <= key[1]:
-                value.append(weight)
+        for weight in self.data:
+            for key, value in sorted_data.items():
+                if weight >= key[0] and weight <= key[1]:
+                    value.append(weight)
+        # makes tuple keys as string so json dumps can save the dictionary
+        sorted_data = {str(key): value for key, value in sorted_data.items()}
+        return sorted_data
 
-    return sorted_data
-
-sorted = make_ranks(data, 10)
-print(sorted)
-
-
-
+# if __name__ == "__main__":
+#     data = stats.uniform.rvs(size=100, loc = 40, scale = 80)
+#     mu, std = stats.norm.fit(data)
+#     datasort = DataSort(data)
+#     sorted_data = datasort.sortrank(10)
+#     print(sorted_data)
 
 
 
