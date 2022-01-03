d4e751e88214a11eadf57b427b4569375ae3031a
Yuchen Huang <yuchen520@live.nl>
2022-01-03 16:22:00 +0100

    Removing unwanted scripts

--- tools.py
+++
@@ -1,8 +0,0 @@
-def without_keys(dictionary, keys):
-    """
-    makes a new dictionary excluding a given key
-    :param dictionary: original dictionary
-    :param keys: key(s) value in dictionary
-    :return: a new dictionary without the give key(s)
-    """
-    return {x: dictionary[x] for x in dictionary if x not in keys}
