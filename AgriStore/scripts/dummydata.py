722b76ce62a40b6c99c389954f96c796fc95869c
Yuchen Huang <yuchen520@live.nl>
2022-01-13 19:23:53 +0100

    minor changes

--- AgriStore/scripts/dummydata.py
+++ AgriStore/scripts/dummydata.py
@@ -1,8 +1,11 @@
 from numpy import pi
 import scipy.stats as stats
 import pickle
+from blockchain_scripts.Data_filtering import *
 
 data = stats.uniform.rvs(size=100, loc=40, scale=80, random_state=9)
+df = Datafilter(data)
+print(df.sortrank(10))
 
-with open("D:\Blockchain\data_scan\\dummydata.txt", "wb") as file:
+with open("D:\Blockchain\data_scan\\dummydata2.txt") as file:
     pickle.dump(data, file)
