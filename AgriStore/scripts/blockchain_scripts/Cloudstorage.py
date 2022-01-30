84f7ab49c6aa038d7d50596e521650022f7ab6e8
Yuchen Huang <yuchen520@live.nl>
2022-01-30 16:32:08 +0100

    added new function ipfs

--- AgriStore/scripts/blockchain_scripts/Cloudstorage.py
+++ AgriStore/scripts/blockchain_scripts/Cloudstorage.py
@@ -1,5 +1,8 @@
+import datetime
+import json
+from pydoc import cli
+from datetime import date
 import dropbox
-import logging
 import os
 import ipfshttpclient
 
@@ -31,4 +34,53 @@ class Google_drive:
 
 
 class ipfs:
-    pass
+    def __init__(self):
+        self.client = ipfshttpclient.connect(session=True)
+        self.rootpath = ""
+
+    def init_maps(self, path, map_name="ifps"):
+        map_name = "ipfs"
+        map_hash = "hash_info"
+        map_storage = "cloud_storage"
+        if not os.path.exists(f"{path}\{map_name}"):
+            os.makedirs(f"{path}\{map_name}")
+        self.rootpath = f"{path}\{map_name}"
+
+        if not os.path.exists(f"{self.rootpath}\{map_storage}"):
+            os.makedirs(f"{self.rootpath}\{map_storage}")
+
+    def clientid(self):
+        return self.client.id()
+
+    def version(self):
+        print(self.client.version())
+
+    def uploadfile(self):
+        upload = self.client.add(f"{self.rootpath}\cloud_storage", recursive=True)
+        info_save = [dict(info) for info in upload]
+
+        if not os.path.exists(f"{self.rootpath}\hash_info"):
+            os.makedirs(f"{self.rootpath}\hash_info")
+
+        info = {
+            "date(yy/mm/dd)": str(datetime.date.today()),
+            "time": str(datetime.datetime.now().time()),
+            "file_info": info_save,
+        }
+        time = str(datetime.datetime.now()).replace(":", "-").replace(" ", "--")
+        # save the info in a file
+        with open(f"{self.rootpath}\hash_info\{str(time)}.txt", "w") as file:
+            file.write(json.dumps(info))
+
+    def viewfile(self, hash):
+        view = self.client.cat(hash).decode("UTF-8")
+        print(view)
+
+    def close(self):
+        self.client.close()
+
+
+client = ipfs()
+client.init_maps("D:\Blockchain")
+client.uploadfile()
+# client.viewfile("QmQVZakc4EEmdZpFgP9PgWGPPkyDwphBraf3prLUpi9hgq")
